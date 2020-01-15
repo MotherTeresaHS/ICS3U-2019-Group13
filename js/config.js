0e94d1c6678581caec84d8f4c00e3cee5ad676f9
Ubuntu <ubuntu@ip-172-31-39-215.ec2.internal>
2020-01-15 19:24:58 +0000

    changed a bit

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_8bSmZnCUx', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_hPGm7dCYF', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-1', // e.g. us-east-2
-		clientId: '59nkjs8te761se7s8vq8p3sci8' //is this used anywhere?
+		clientId: '1n1n5c7psbg9p4vho0p0a8ghqt' //is this used anywhere?
     },
 };
