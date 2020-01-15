e9112823a983876a0013a4ca1bfc537966c30fe8
school pc <amir.mersad@mths.ca>
2020-01-15 14:35:26 -0500

    cognito working again

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_hPGm7dCYF', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_lauWkAmSz', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-1', // e.g. us-east-2
-		clientId: '1n1n5c7psbg9p4vho0p0a8ghqt' //is this used anywhere?
+		clientId: '51grb06boi9p91skh4mo30n9pb' //is this used anywhere?
     },
 };
