e02082fdd12a0ca23c903685ed072f6aca1131f1
school pc <amir.mersad@mths.ca>
2020-01-22 13:54:33 -0500

    indented all lines

--- js/profile.js
+++ js/profile.js
@@ -1,74 +1,70 @@
 // JavaScript File
-		
+
 async function getUser(email_address) {
-    // get the user info from API Gate
-    
-    const api_url = 'https://73m0qekp2j.execute-api.us-east-1.amazonaws.com/prod/user-profile?email_address=' + email_address;
-    const api_response = await fetch(api_url);
-    const api_data = await(api_response).json();
-    console.log(api_data);
-    
-    const json_profile = JSON.parse(api_data['body']);
-    const json_profile_body = JSON.parse(json_profile['body']);
-    const div_user_profile_email = document.getElementById('profile_email');
-    const div_user_profile_first_name = document.getElementById('profile_first_name');
-    const div_user_profile_last_name = document.getElementById('profile_last_name');
-    const div_user_profile_period_one = document.getElementById('profile_period_one');
-	const div_user_profile_period_two = document.getElementById('profile_period_two');
-    const div_user_profile_period_three = document.getElementById('profile_period_three');
-    const div_user_profile_period_four = document.getElementById('profile_period_four');
+  // get the user info from API Gate
 
-    
+  const api_url = 'https://73m0qekp2j.execute-api.us-east-1.amazonaws.com/prod/user-profile?email_address=' + email_address;
+  const api_response = await fetch(api_url);
+  const api_data = await (api_response).json();
+  console.log(api_data);
 
-    div_user_profile_email.innerHTML = json_profile['email'];
-    div_user_profile_first_name.innerHTML = json_profile['first_name'];
-    div_user_profile_last_name.innerHTML = json_profile['last_name'];
-    div_user_profile_period_one.innerHTML = json_profile_body['period1'];
-    div_user_profile_period_two.innerHTML = json_profile_body['period2'];
-    div_user_profile_period_three.innerHTML = json_profile_body['period3'];
-    div_user_profile_period_four.innerHTML = json_profile_body['period4'];
+  const json_profile = JSON.parse(api_data['body']);
+  const json_profile_body = JSON.parse(json_profile['body']);
+  const div_user_profile_email = document.getElementById('profile_email');
+  const div_user_profile_first_name = document.getElementById('profile_first_name');
+  const div_user_profile_last_name = document.getElementById('profile_last_name');
+  const div_user_profile_period_one = document.getElementById('profile_period_one');
+  const div_user_profile_period_two = document.getElementById('profile_period_two');
+  const div_user_profile_period_three = document.getElementById('profile_period_three');
+  const div_user_profile_period_four = document.getElementById('profile_period_four');
 
-	console.log(div_user_profile_first_name);
-	console.log(div_user_profile_last_name);
-	console.log(div_user_profile_period_one);
-	console.log(div_user_profile_period_two);
-	console.log(div_user_profile_period_three);
-	console.log(div_user_profile_period_four);
-	console.log(json_profile);
-    
 
-  }
-  
-  
-  
+  div_user_profile_email.innerHTML = json_profile['email'];
+  div_user_profile_first_name.innerHTML = json_profile['first_name'];
+  div_user_profile_last_name.innerHTML = json_profile['last_name'];
+  div_user_profile_period_one.innerHTML = json_profile_body['period1'];
+  div_user_profile_period_two.innerHTML = json_profile_body['period2'];
+  div_user_profile_period_three.innerHTML = json_profile_body['period3'];
+  div_user_profile_period_four.innerHTML = json_profile_body['period4'];
+
+  console.log(div_user_profile_first_name);
+  console.log(div_user_profile_last_name);
+  console.log(div_user_profile_period_one);
+  console.log(div_user_profile_period_two);
+  console.log(div_user_profile_period_three);
+  console.log(div_user_profile_period_four);
+  console.log(json_profile);
+}
+
+
 function getUserAttributes() {
-	var data = { 
-		UserPoolId : _config.cognito.userPoolId,
-    ClientId : _config.cognito.clientId
-	};
-	var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
-	var cognitoUser = userPool.getCurrentUser();
+  var data = {
+    UserPoolId: _config.cognito.userPoolId,
+    ClientId: _config.cognito.clientId
+  };
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
+  var cognitoUser = userPool.getCurrentUser();
 
-	if (cognitoUser != null) {
-  	cognitoUser.getSession(function(err, session) {
+  if (cognitoUser != null) {
+    cognitoUser.getSession(function (err, session) {
       if (err) {
-      	alert(err);
+        alert(err);
         return;
       }
       //console.log('session validity: ' + session.isValid());
-      
-      cognitoUser.getUserAttributes(function(err, result) {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				// user email address
-				console.log(result[2].getValue());
-				getUser(result[2].getValue()) 
-			});
 
-  	});
-	} else {
-		console.log("Already signed-out")
-	}
+      cognitoUser.getUserAttributes(function (err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        // user email address
+        console.log(result[2].getValue());
+        getUser(result[2].getValue())
+      });
+
+    });
+  } else {
+    console.log("Already signed-out")
+  }
 }
--- js/sign-in.js
+++ js/sign-in.js
@@ -2,31 +2,31 @@
 
 function signInButton() {
   // sign-in to AWS Cognito
-  
-  var data = { 
-	  UserPoolId : _config.cognito.userPoolId,
-    ClientId : _config.cognito.clientId
+
+  var data = {
+    UserPoolId: _config.cognito.userPoolId,
+    ClientId: _config.cognito.clientId
   };
   var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
   var cognitoUser = userPool.getCurrentUser();
 
-	var authenticationData = {
-    Username : document.getElementById("inputUsername").value,
-    Password : document.getElementById("inputPassword").value,
+  var authenticationData = {
+    Username: document.getElementById("inputUsername").value,
+    Password: document.getElementById("inputPassword").value,
   };
 
   var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
 
   var poolData = {
-    UserPoolId : _config.cognito.userPoolId, // Your user pool id here
-    ClientId : _config.cognito.clientId, // Your client id here
+    UserPoolId: _config.cognito.userPoolId, // Your user pool id here
+    ClientId: _config.cognito.clientId, // Your client id here
   };
 
   var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
   var userData = {
-    Username : document.getElementById("inputUsername").value,
-    Pool : userPool,
+    Username: document.getElementById("inputUsername").value,
+    Pool: userPool,
   };
 
   var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
@@ -34,23 +34,23 @@ function signInButton() {
   cognitoUser.authenticateUser(authenticationDetails, {
     onSuccess: function (result) {
       var accessToken = result.getAccessToken().getJwtToken();
-      console.log(result);	
-      
+      console.log(result);
+
       //get user info, to show that you are logged in
-			cognitoUser.getUserAttributes(function(err, result) {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				console.log(result);
-				document.getElementById("logged-in").innerHTML = "You are logged in as: " + result[2].getValue();
-				
-				// now auto redirect to profile page
-				window.location.replace("./profile.html");
-			});
-      
+      cognitoUser.getUserAttributes(function (err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+        document.getElementById("logged-in").innerHTML = "You are logged in as: " + result[2].getValue();
+
+        // now auto redirect to profile page
+        window.location.replace("./profile.html");
+      });
+
     },
-    onFailure: function(err) {
+    onFailure: function (err) {
       alert(err.message || JSON.stringify(err));
     },
   });
